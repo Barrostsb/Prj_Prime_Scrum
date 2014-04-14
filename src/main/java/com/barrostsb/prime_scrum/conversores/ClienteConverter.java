7b4ddd14a468895ad3a5aaeaedf0f713d1f8b7f1
Thiago <Thiago@192.168.209.20>
2014-04-14 20:32:27 -0300

    Conversor de cliente

---
+++ src/main/java/com/barrostsb/prime_scrum/conversores/ClienteConverter.java
@@ -0,0 +1,39 @@
+package com.barrostsb.prime_scrum.conversores;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.persistence.EntityManager;
+
+import com.barrostsb.prime_scrum.JpaUtils.JpaUtils;
+import com.barrostsb.prime_scrum.model.Cliente;
+import com.barrostsb.prime_scrum.model.Desenvolvedor;
+import com.barrostsb.prime_scrum.model.Pessoa;
+import com.barrostsb.prime_scrum.repository.Pessoas;
+
+@FacesConverter(value="clienteConverter" , forClass = Pessoa.class)
+public class ClienteConverter implements Converter {
+	@Override
+	public Object getAsObject(FacesContext context,	UIComponent component, String value) {
+		Pessoa retorno = null;
+		EntityManager manager = JpaUtils.getEntityManager();
+		try {
+			if (value != null) {
+				Pessoas pessoas = new Pessoas(manager);
+				retorno = (Cliente)pessoas.PessoaPorLogin(value);
+//				retorno = pessoas.PessoaPorId(new Integer(value));
+			}
+			return retorno;
+		} finally {
+			manager.close();
+		}
+	}
+	@Override
+	public String getAsString(FacesContext context,	UIComponent component, Object value) {
+		if (value != null) {
+			return ((Pessoa) value).getLogin().toString();
+		}
+		return null;
+	}
+}
