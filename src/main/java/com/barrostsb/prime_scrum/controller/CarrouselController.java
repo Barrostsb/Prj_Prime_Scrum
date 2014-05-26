ed123f1bb1efe419e58e0236034e2fbaca6f7cb8
Thiago <Thiago@Thiago-PC>
2014-05-26 00:20:46 -0300

    Alteração nas imagens

--- src/main/java/com/barrostsb/prime_scrum/controller/CarrouselController.java
+++ src/main/java/com/barrostsb/prime_scrum/controller/CarrouselController.java
@@ -1,6 +1,7 @@
 package com.barrostsb.prime_scrum.controller;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -19,6 +20,7 @@ public class CarrouselController {
 	    public void init() {
 	        createScreenShots();
 	        createTecnologias();
+	        
 	    }
 	 
 	   
@@ -34,57 +36,70 @@ public class CarrouselController {
 	    
 	    public List<ScreenShots> createScreenShots(){
 	    	images = new ArrayList<ScreenShots>();
-	    	ScreenShots t1 = new ScreenShots();
-	    	t1.setNome("1.png");
-	    	t1.setTitulo("SCRUM TASK BOARD");
-	    	t1.setDescricao("task");
+	    	ScreenShots s1 = new ScreenShots();
+	    	s1.setNome("1.png");
+	    	s1.setTitulo("Prime Scrum");
+	    	s1.setDescricao("Gerencie seus projetos com SCRUM utilizando esta incrível ferramenta!");
 	    	
-	    	ScreenShots t2 = new ScreenShots();
-	    	t2.setNome("2.png");
-	    	t2.setTitulo("Eclipse IDE");
-	    	t2.setDescricao("Eclipse IDE Kepler");
+	    	ScreenShots s2 = new ScreenShots();
+	    	s2.setNome("2.png");
+	    	s2.setTitulo("Acesso rápido");
+	    	s2.setDescricao("Veja o status de seu projeto rapidamente! ");
 	    	
-	    	ScreenShots t3 = new ScreenShots();
-	    	t3.setNome("3.png");
-	    	t3.setTitulo("JFS");
-	    	t3.setDescricao("Java Server Faces");
+	    	ScreenShots s3 = new ScreenShots();
+	    	s3.setNome("3.png");
+	    	s3.setTitulo("Listagem de Stackholders");
+	    	s3.setDescricao("O Prime Scrum lista para você todos os seus clientes e desenvolvedores!");
 	    	
-//	    	Tecnologias t4 = new Tecnologias();
-//	    	t4.setNome("4.png");
-//	    	t4.setTitulo("Prime Faces");
-//	    	t4.setDescricao("Implementação do jsf");
-//	    	
-//	    	Tecnologias t5 = new Tecnologias();
-//	    	t5.setNome("5.png");
-//	    	t5.setTitulo("Hibernate");
-//	    	t5.setDescricao("hibernate ");
-//	    	
-//	    	Tecnologias t6 = new Tecnologias();
-//	    	t1.setNome("6.png");
-//	    	t1.setTitulo("Mysql");
-//	    	t1.setDescricao("SGBD");
-//	    	
-//	    	Tecnologias t7 = new Tecnologias();
-//	    	t1.setNome("7.png");
-//	    	t1.setTitulo("Gpl3");
-//	    	t1.setDescricao("software livre");
+	    	ScreenShots s4 = new ScreenShots();
+	    	s4.setNome("4.png");
+	    	s4.setTitulo("Listagem de Projetos");
+	    	s4.setDescricao("Encontre seus projetos de uma forma rápida e fácil!");
 	    	
-	    	images.add(t1);
-	    	images.add(t2);
-	    	images.add(t3);
-//	    	tecnologias.add(t4);
-//	    	tecnologias.add(t5);
-//	    	tecnologias.add(t6);
-//	    	tecnologias.add(t7);
+	    	ScreenShots s5 = new ScreenShots();
+	    	s5.setNome("5.png");
+	    	s5.setTitulo("Listagem de Tarefa");
+	    	s5.setDescricao("Veja suas tarefas com um visual de bilhetes!");
+	    	
+	    	ScreenShots s6= new ScreenShots();
+	    	s6.setNome("6.png");
+	    	s6.setTitulo("Listagem de Tarefas");
+	    	s6.setDescricao("Veja e altere rapidamente suas tarefas");
+	    	
+	    	ScreenShots s7 = new ScreenShots();
+	    	s7.setNome("7.png");
+	    	s7.setTitulo("Scrum Task Board");
+	    	s7.setDescricao("O Prime Scrum possui uma task board para facilitar a visualização de suas tarefas! ");
+	    	
+	    	ScreenShots s8 = new ScreenShots();
+	    	s8.setNome("8.png");
+	    	s8.setTitulo("Burndown Chart");
+	    	s8.setDescricao("O Prime Scrum possui uma burn down chart para facilitar o gerenciamento do tempo para o termini de seus projetos!");
+	    	
+	    	ScreenShots s9 = new ScreenShots();
+	    	s9.setNome("9.png");
+	    	s9.setTitulo("Ele é livre");
+	    	s9.setDescricao("O Prime Scrum é livre, altere como desejar!");
+	    	
+	    	images.add(s1);
+	    	images.add(s2);
+	    	images.add(s3);
+	    	images.add(s4);
+	    	images.add(s5);
+	    	images.add(s6);
+	    	images.add(s7);
+	    	images.add(s8);
+	    	images.add(s9);
 	    	return images;
 	    }
 		
 		 public List<Tecnologias> createTecnologias(){
 		    	tecnologias = new ArrayList<Tecnologias>();
+		    	
 		    	Tecnologias t1 = new Tecnologias();
 		    	t1.setNome("1.png");
-		    	t1.setTitulo("Linguagem Java");
-		    	t1.setDescricao("Linguagem Java 1.7");
+		    	t1.setTitulo("Linguagem java");
+		    	t1.setDescricao("java 7");
 		    	
 		    	Tecnologias t2 = new Tecnologias();
 		    	t2.setNome("2.png");
@@ -107,14 +122,14 @@ public class CarrouselController {
 		    	t5.setDescricao("hibernate ");
 		    	
 		    	Tecnologias t6 = new Tecnologias();
-		    	t1.setNome("6.png");
-		    	t1.setTitulo("Mysql");
-		    	t1.setDescricao("SGBD");
+		    	t6.setNome("6.png");
+		    	t6.setTitulo("Mysql");
+		    	t6.setDescricao("mysql oracle");
 		    	
 		    	Tecnologias t7 = new Tecnologias();
-		    	t1.setNome("7.png");
-		    	t1.setTitulo("Gpl3");
-		    	t1.setDescricao("software livre");
+		    	t7.setNome("7.png");
+		    	t7.setTitulo("Gpl3");
+		    	t7.setDescricao("software livre");
 		    	
 		    	tecnologias.add(t1);
 		    	tecnologias.add(t2);
